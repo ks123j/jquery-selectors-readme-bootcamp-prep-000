cffe7eb4986563a8d0698efc4b89cd8430766900
Katy <komal.vadukul@gmail.com>
2017-11-27 19:43:02 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector(){
+  return   $('p');
+}
+
+function lastImageSelector(){
+  return   $('div ul: last');
+}
+
+function ninjaBabySelector(){
+  return   $('#baby-ninja');
+}
+
+function divSelector(){
+  return   $('.pics');
+}
+
+function firstListItem(){
+var pics =  $('ul li');
+return (pics[0]);
+}
